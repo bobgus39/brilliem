0e152fc6e71fe36fb9956d06f5ced7b57ed4af32
macagus <bobgus39@gmail.com>
2025-11-26 17:43:58 +0100

    upload browse

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
-  base: 'https://brilliem.com',
+  
 });
+
+
+//base: 'https://bobgus39.github.io/brilliem.com',
